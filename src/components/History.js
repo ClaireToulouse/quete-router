3bd991b9316708fba4a40c6bc194ed1886bab3d9
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-15 15:44:39 +0200

    quete router terminée

---
+++ src/components/History.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const History = props => (
+  <div className="mt-5">
+    <h3>Notre histoire ....</h3>
+    <p className="align-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque imperdiet purus eget porttitor. Sed et molestie elit. Donec volutpat augue lorem, quis pellentesque ipsum vehicula et. Sed tempus purus at pretium blandit. Quisque a lacinia est. Morbi suscipit justo nunc. In hac habitasse platea dictumst. Phasellus eros lectus, dignissim vitae viverra quis, egestas id tellus. Sed sed iaculis diam.
+      Donec mollis ultrices tellus, in aliquet arcu maximus eu. In id mauris porttitor sapien ullamcorper vehicula. Quisque luctus massa vitae eleifend pulvinar. Etiam leo leo, consequat in mattis id, sagittis eget orci. Aenean sed tincidunt augue. Morbi maximus eros nec nisi lacinia, sit amet ullamcorper lacus feugiat. Proin a erat leo. Donec porta, purus sed dapibus euismod, ex mauris dapibus nisi, eget aliquam nisl risus id nulla. Donec in pulvinar tellus, quis egestas neque. Suspendisse fermentum leo eget mauris consequat iaculis non eget sapien. Sed malesuada est sed varius consequat.
+    </p>
+  </div>
+);
+
+export default History;
